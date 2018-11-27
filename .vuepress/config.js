ba0ee2c39042d4fafaecdd884faf043538242f7b
Ali Bashi <alisalad123@gmail.com>
2018-11-28 01:40:44 +1100

    static

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Dastuurka",
+  description: "Jamhuuriyadda Federaalka ee Soomaaliya",
+  head: [["link", { rel: "icon", href: "/images/Somalia.png" }]],
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Dastuurka", link: "/dastuurka/" },
+      { text: "Guide", link: "/guide/" }
+    ],
+    sidebar: "auto"
+  }
+};
